51631d929ad1a147a83d0fefbcd50d3476572b63
Fernando Guzman Lugo <lugo.fernando@gmail.com>
2015-03-12 02:36:27 -0700

    Creating TCP server script
    Initial update of the TCP server for Intel Edison Controlller.
    
    This server is in charge of handling resource request from
    IntelEdisonController which is an Android App.
    
    Right now, it only has support for GPIOs and only for writing values
    
    Signed-off-by: Fernando Guzman Lugo <lugo.fernando@gmail.com>

---
+++ IntelEdisonTcpServer.py
@@ -0,0 +1,59 @@
+import SocketServer
+import mraa
+import json
+import sys
+
+class IntelEdisonThreadedServer(SocketServer.BaseRequestHandler):
+    def setup(self):
+        print (self.client_address, 'connected!')
+
+    def handle(self):
+        while 1:
+            if handle_request(self.request) == 0:
+                return
+
+    def finish(self):
+        print (self.client_address, 'disconnected!')
+
+# GPIO_PATH='/sys/kernel/debug/gpio_debug/'
+
+def gpio_handler(n, d, v):
+    # Use mraa library instead of sysfs
+    x = mraa.Gpio(n)
+    if d == 'out':
+        x.dir(mraa.DIR_OUT)
+        x.write(v)
+    elif d == 'in':
+        x.dir(mraa.DIR_IN)
+    else:
+        print "Invalid dir option" # this should never happen
+
+def gpio_parser(j):
+    print (json.dumps(j, sort_keys=True, indent=4, separators=(',', ':')))
+    gpio_handler(j['num'], j['dir'], j['value'])
+
+def json_parser(j):
+    if 'type' in j:
+        if j['type'] == 'gpio':
+            gpio_parser(j)
+        else:
+            print 'type ' + j['type'] + ' no supported'
+        return j
+
+
+def handle_request(request):
+    data = request.recv(1024)
+    if not data: return 0
+    print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ':')))
+    json.loads(data, object_hook=json_parser)
+
+    return 1
+
+
+if len(sys.argv) > 1 :
+    port = sys.argv[1]
+else:
+    port = "8086" #default port
+
+# start server forever
+SocketServer.ThreadingTCPServer(('', int(port)), IntelEdisonThreadedServer).serve_forever()
