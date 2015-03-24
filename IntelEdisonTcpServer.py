d978dfca9e04305f6050781a202dac8e0a9cfe1e
Fernando Guzman Lugo <lugo.fernando@gmail.com>
2015-03-23 23:51:41 -0700

    Revert "Read socket one line at the time"
    seems there are some issues with makefile function and some python
    versions. Revert for now until I get an efficient way to program this
    
    This reverts commit f09467fb34e44e7266ffc99f48f873e20bf953f4.
    
    Change-Id: I6202f864123a5c54ed6cecb2643f84d3ed58713b
    Signed-off-by: Fernando Guzman Lugo <lugo.fernando@gmail.com>

--- IntelEdisonTcpServer.py
+++ IntelEdisonTcpServer.py
@@ -44,10 +44,8 @@ def json_parser(j):
 def handle_request(request):
     data = request.recv(1024)
     if not data: return 0
-    f = data.makefile()
-    for l in f.readlines():
-        print (json.dumps(l, sort_keys=True, indent=4, separators=(',', ':')))
-        json.loads(l, object_hook=json_parser)
+    print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ':')))
+    json.loads(data, object_hook=json_parser)
 
     return 1
 
